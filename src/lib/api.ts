7173c4f73b31c340fd94651fd065e7ee46a04f78
martinmorag <113954312+martinmorag@users.noreply.github.com>
2024-07-16 17:17:00 -0300

    Update api.ts

--- src/lib/api.ts
+++ src/lib/api.ts
@@ -1,1 +1,1 @@
-export const API_BASE_URL = process.env.NODE_ENV === "production" ? ('https://group-project-z4hc.onrender.com' || process.env.API_BASE_URL) : 'http://localhost:4000';
+export const API_BASE_URL = process.env.NODE_ENV === "production" ? ('https://ecommerce-server-6fyn.onrender.com' || process.env.API_BASE_URL) : 'http://localhost:4000';
