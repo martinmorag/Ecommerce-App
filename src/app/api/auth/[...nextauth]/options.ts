a14b9b4e7b9adccd19607aba9762e8eb9be209b8
Martin Moraga <moragamartin2@gmail.com>
2024-12-10 12:23:41 -0300

    Added Google login

--- src/app/api/auth/[...nextauth]/options.ts
+++ src/app/api/auth/[...nextauth]/options.ts
@@ -1,5 +1,7 @@
-import type { NextAuthOptions } from 'next-auth'
-import GitHubProvider from 'next-auth/providers/github'
+import type { NextAuthOptions } from 'next-auth';
+import GitHubProvider from 'next-auth/providers/github';
+import GoogleProvider from 'next-auth/providers/google';
+
 // import CredentialsProvider from 'next-auth/providers/credentials'
 
 export const options: NextAuthOptions = {
@@ -7,6 +9,10 @@ export const options: NextAuthOptions = {
         GitHubProvider({
             clientId: process.env.GITHUB_ID as string,
             clientSecret: process.env.GITHUB_SECRET as string,
+        }),
+        GoogleProvider({
+            clientId: process.env.GOOGLE_CLIENT_ID as string,
+            clientSecret: process.env.GOOGLE_CLIENT_SECRET as string,
         }),
         // CredentialsProvider({
         //     name: "Credentials",
